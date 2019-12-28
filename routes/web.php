6c9db6ae969ddbb320dcd5e9718d84fed62f1657
Vishal <itzmevishu@gmail.com>
2019-12-28 18:48:52 +0530

    First Commit

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'BlogController@getPosts');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+// Get all published posts
+Route::get('blog', 'BlogController@getPosts');
+
+// Get posts for a given tag
+Route::get('tag/{slug}', 'BlogController@getPostsByTag');
+
+// Get posts for a given topic
+Route::get('topic/{slug}', 'BlogController@getPostsByTopic');
+
+// Find a single post
+Route::middleware('Canvas\Http\Middleware\ViewThrottle')->get('{slug}', 'BlogController@findPostBySlug');
+
+Route::prefix('blog')->group(function () {
+    Route::get('/', 'BlogController@getPosts')->name('blog.index');
+    Route::middleware('Canvas\Http\Middleware\ViewThrottle')->get('{slug}', 'BlogController@findPostBySlug')->name('blog.post');
+    Route::get('tag/{slug}', 'BlogController@getPostsByTag')->name('blog.tag');
+    Route::get('topic/{slug}', 'BlogController@getPostsByTopic')->name('blog.topic');
+});
